ebbdb20be1a2a217756e8aa46da7857f01de5593
Morphei <xain3000@gmail.com>
2021-04-21 21:33:58 +0400

    правка 1

--- src/components/product-card/components/bag/ajax.js
+++ src/components/product-card/components/bag/ajax.js
@@ -1,35 +1,35 @@
-// window.addEventListener('DOMContentLoaded', () => {
-const basket = document.querySelector('.bag__button');
-basket.addEventListener('click', sendServer);
-function sendServer(e) {
-  e.preventDefault();
-  const phone = document.querySelector('.order__input').value;
-  const article = document.querySelector('.info__input').value;
-  const size = document.querySelector('.size__button_active').value;
-  const count = document.querySelector('.bag__input').value;
+window.addEventListener('DOMContentLoaded', () => {
+  const basket = document.querySelector('.bag__basket');
+  console.log(basket)
+  basket.addEventListener('click', sendServer);
+  function sendServer(e) {
+    e.preventDefault();
+    const phone = document.querySelector('.order__input').value;
+    const article = document.querySelector('.info__input').value;
+    const size = document.querySelector('.size__button_active').value;
+    const count = document.querySelector('.bag__input').value;
 
-  const data = new FormData();
-  data.append('phone', phone);
-  data.append('article', article);
-  data.append('size', size);
-  data.append('count', count);
-
-  console.log(data)
-  fetch('./post.php',
-    {
-      method: 'POST',
-      body: data,
-    })
-    .then(response => {
-      if (response.status !== 200) {
-        return Promise.reject();
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(JSON.stringify(data))
-      console.log(data)
-    })
-    .catch(() => console.log('ошибка'));
-}
-// })
+    const data = new FormData();
+    data.append('phone', phone);
+    data.append('article', article);
+    data.append('size', size);
+    data.append('count', count);
+    console.log(data)
+    fetch('https://my-json-server.typicode.com/lMorpheil/serv/posts',
+      {
+        method: 'POST',
+        body: data,
+      })
+      .then(response => {
+        if (response.status !== 200) {
+          return Promise.reject();
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(JSON.stringify(data))
+        console.log(data)
+      })
+      .catch(() => console.log('ошибка'));
+  }
+})
--- src/components/product-card/product-card.js
+++ src/components/product-card/product-card.js
@@ -3,6 +3,6 @@ import './components/info/info';
 import './components/size/size';
 import './components/description/description';
 import './components/bag/bag';
+import './components/bag/ajax';
 import './components/order/order';
 import './components/picture/picture';
-import './components/bag/ajax';
