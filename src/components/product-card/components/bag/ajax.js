7edfdba3f34607f775ce53c17a87364996cf05d6
Morphei <xain3000@gmail.com>
2021-04-21 20:29:13 +0400

    исправил небольшие погрешности

--- src/components/product-card/components/bag/ajax.js
+++ src/components/product-card/components/bag/ajax.js
@@ -1,39 +1,35 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const basket = document.querySelector('.bag__button');
-  if (basket) {
-    basket.addEventListener('click', send_mail)
-  }
-  function send_mail(e) {
-    e.preventDefault()
-    let phone = document.querySelector('.order__input').value;
-    let article = document.querySelector('.info__input').value;
-    let size = document.querySelector('.size__button_active').value;
-    let count = document.querySelector('.bag__input').value;
+// window.addEventListener('DOMContentLoaded', () => {
+const basket = document.querySelector('.bag__button');
+basket.addEventListener('click', sendServer);
+function sendServer(e) {
+  e.preventDefault();
+  const phone = document.querySelector('.order__input').value;
+  const article = document.querySelector('.info__input').value;
+  const size = document.querySelector('.size__button_active').value;
+  const count = document.querySelector('.bag__input').value;
 
-    let data = new FormData()
-    data.append('phone', phone)
-    data.append('article', article)
-    data.append('size', size)
-    data.append('count', count)
+  const data = new FormData();
+  data.append('phone', phone);
+  data.append('article', article);
+  data.append('size', size);
+  data.append('count', count);
 
-    console.log(data)
-    fetch('./post.php',
-      {
-        method: "POST",
-        body: data
-      })
-      .then(response => {
-        if (response.status !== 200) {
-          return Promise.reject();
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(JSON.stringify(data))
-        console.log(data)
-      })
-      .catch(() => console.log('ошибка'));
-  }
-
-
-})
+  console.log(data)
+  fetch('./post.php',
+    {
+      method: 'POST',
+      body: data,
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        return Promise.reject();
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(JSON.stringify(data))
+      console.log(data)
+    })
+    .catch(() => console.log('ошибка'));
+}
+// })
